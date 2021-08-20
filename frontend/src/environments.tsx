9983e365dd63db8375c117eb2300fe976b46341f
devup2332 <devup2332@gmail.com>
2021-08-20 12:44:39 -0500

    Fixed 2 bugs of frontend 1. Tags Input 2. Delete Photo

--- frontend/src/environments.tsx
+++ frontend/src/environments.tsx
@@ -1,6 +1,6 @@
 
 export const environments = {
-  api_uri: "/api",
+  api_uri: "http://localhost:8000/api",
   email_pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
   number_pattern: /^[0-9]+$/,
 };
